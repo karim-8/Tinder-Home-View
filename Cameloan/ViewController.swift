98b3f8165fe44bd87b4f87d4dab1324c47412a4b
kariim8 <KariimAhmed8@gmail.com>
2019-10-17 16:51:30 +0200

    Initial Commit

---
+++ Cameloan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cameloan
+//
+//  Created by KARIM on 10/17/19.
+//  Copyright © 2019 KARIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
